06cf9367acddceec7c49847168e6cb4d4e2b54e4
Pranav <npv12@iitbbs.ac.in>
2022-07-23 12:42:01 +0530

    chore: Update upload path for gapps
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- bot/document_processor/base.py
+++ bot/document_processor/base.py
@@ -45,7 +45,7 @@ class DocumentProccesor(ABC):
         # Upload path of the final file
         device = find_device(file_name)
         file_upload_path = "flamingo/A12.1/" + device + \
-            "/Vanilla" if is_build_vanilla(file_name) else "/Gapps"
+            "/Vanilla" if is_build_vanilla(file_name) else "/GApps"
 
         try:
             # Use the context manager to manage a session instance
