48fa608a05f5db3ccfa52dc8f87c3de95ab2420d
Pranav <npv12@iitbbs.ac.in>
2022-10-25 10:53:03 +0530

    chore: update help

--- plugins/help.go
+++ plugins/help.go
@@ -11,11 +11,21 @@ func helpHandler(b *gotgbot.Bot, ctx *ext.Context) error {
 	core.Log.Infoln("Recieved request to handle /help")
 	var helpTxt = `
 	Hey, I'm Flamingo upload bot. I upload OTA files from your drives to hosted drive
-	/cancel ID - Cancel an upload
+	/cancel <ID> - Cancel an upload
 	/help - Show this message
 	/ping - Check how slow I am today ;)
 	/release - Upload a release
 	/start - Check if I am alive? 
+
+	These below are specific to maintainers. Alternatively admins can use by replying to a user
+	/addGroup <link> - add a support group link
+	/dropDevice <devices separated by space> - drop device from maintainence
+	/remove - to drop yourself from maintainer status
+
+	These below are specific to admins
+	/add <devices> - add a new maintainer. Reply to the user
+	/demote - demote the user being replied to an admin
+	/promote - promote the user being replied to an admin
 	`
 	_, e := b.SendMessage(chat.Id, helpTxt, &gotgbot.SendMessageOpts{})
 	return e
