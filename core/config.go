baba00a8f152a33bfb2555b1fa67a1df89f9a7aa
Pranav <npv12@iitbbs.ac.in>
2022-10-28 20:59:20 +0530

    chore: config: add ondrive creds

--- core/config.go
+++ core/config.go
@@ -16,6 +16,10 @@ type BotConfig struct {
 	GDriveRefreshToken      string `json:"gdrive_refresh_token"`
 	GDriveTokenType         string `json:"gdrive_token_type"`
 	GDriveExpiry            string `json:"gdrive_expiry"`
+	OneDriveClientId        string `json:"onedrive_client_id"`
+	OneDriveClientSecret    string `json:"onedrive_client_secret"`
+	OneDriveTenantId        string `json:"onedrive_tenant_id"`
+	OneDriveRefreshToken    string `json:"onedrive_refresh_token"`
 }
 
 func NewBotConfig(fileName string) *BotConfig {
