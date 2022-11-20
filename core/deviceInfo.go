cd1089b0b8a4473bb0dd6b359af7523fdd781da8
Pranav <npv12@iitbbs.ac.in>
2022-11-20 17:07:03 +0530

    drop useless buildformat checks

--- core/deviceInfo.go
+++ core/deviceInfo.go
@@ -6,33 +6,17 @@ import (
 )
 
 type DeviceInfo struct {
-	DeviceName  string   `json:"device_name"`
-	Version     string   `json:"version"`
-	BuildDate   string   `json:"build_date"`
-	BuildType   string   `json:"build_type"` // user, userdebug, eng
-	Flavour     string   `json:"flavour"`
-	BuildFormat []string `json:"build_format"` // full, incremental, etc
+	DeviceName string `json:"device_name"`
+	Version    string `json:"version"`
+	BuildDate  string `json:"build_date"`
+	BuildType  string `json:"build_type"` // user, userdebug, eng
+	Flavour    string `json:"flavour"`
 }
 
 func ParseDeviceInfo(fileName []string) (DeviceInfo, string, string, error) {
 	Log.Info("Parsing device info from ", fileName)
-	buildFormats := []string{}
 	fullOtaFile := ""
 	incrementalOtaFile := ""
-	for _, file := range fileName {
-		if strings.Contains(file, "full") {
-			buildFormats = append(buildFormats, "full")
-			fullOtaFile = file
-		} else if strings.Contains(file, "incremental") {
-			buildFormats = append(buildFormats, "incremental")
-			incrementalOtaFile = file
-		} else if strings.Contains(file, "fastboot") {
-			buildFormats = append(buildFormats, "fastboot")
-		} else if strings.Contains(file, "boot") {
-			buildFormats = append(buildFormats, "boot")
-		}
-	}
-
 	if fullOtaFile == "" {
 		return DeviceInfo{}, "", "", fmt.Errorf("full build not found")
 	}
@@ -48,11 +32,10 @@ func ParseDeviceInfo(fileName []string) (DeviceInfo, string, string, error) {
 	version := strings.Replace(deviceDets[1], "v", "", 1)
 
 	return DeviceInfo{
-		Version:     version,
-		DeviceName:  deviceDets[2],
-		BuildType:   deviceDets[3],
-		Flavour:     deviceDets[5],
-		BuildDate:   deviceDets[6],
-		BuildFormat: buildFormats,
+		Version:    version,
+		DeviceName: deviceDets[2],
+		BuildType:  deviceDets[3],
+		Flavour:    deviceDets[5],
+		BuildDate:  deviceDets[6],
 	}, fullOtaFile, incrementalOtaFile, nil
 }
