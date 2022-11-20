3eed88d472faf9bf225ccf28607d8c41476132c5
Pranav <npv12@iitbbs.ac.in>
2022-11-20 23:01:51 +0530

    Revert "drop useless buildformat checks"
    This reverts commit cd1089b0b8a4473bb0dd6b359af7523fdd781da8.

--- core/deviceInfo.go
+++ core/deviceInfo.go
@@ -6,17 +6,33 @@ import (
 )
 
 type DeviceInfo struct {
-	DeviceName string `json:"device_name"`
-	Version    string `json:"version"`
-	BuildDate  string `json:"build_date"`
-	BuildType  string `json:"build_type"` // user, userdebug, eng
-	Flavour    string `json:"flavour"`
+	DeviceName  string   `json:"device_name"`
+	Version     string   `json:"version"`
+	BuildDate   string   `json:"build_date"`
+	BuildType   string   `json:"build_type"` // user, userdebug, eng
+	Flavour     string   `json:"flavour"`
+	BuildFormat []string `json:"build_format"` // full, incremental, etc
 }
 
 func ParseDeviceInfo(fileName []string) (DeviceInfo, string, string, error) {
 	Log.Info("Parsing device info from ", fileName)
+	buildFormats := []string{}
 	fullOtaFile := ""
 	incrementalOtaFile := ""
+	for _, file := range fileName {
+		if strings.Contains(file, "full") {
+			buildFormats = append(buildFormats, "full")
+			fullOtaFile = file
+		} else if strings.Contains(file, "incremental") {
+			buildFormats = append(buildFormats, "incremental")
+			incrementalOtaFile = file
+		} else if strings.Contains(file, "fastboot") {
+			buildFormats = append(buildFormats, "fastboot")
+		} else if strings.Contains(file, "boot") {
+			buildFormats = append(buildFormats, "boot")
+		}
+	}
+
 	if fullOtaFile == "" {
 		return DeviceInfo{}, "", "", fmt.Errorf("full build not found")
 	}
@@ -32,10 +48,11 @@ func ParseDeviceInfo(fileName []string) (DeviceInfo, string, string, error) {
 	version := strings.Replace(deviceDets[1], "v", "", 1)
 
 	return DeviceInfo{
-		Version:    version,
-		DeviceName: deviceDets[2],
-		BuildType:  deviceDets[3],
-		Flavour:    deviceDets[5],
-		BuildDate:  deviceDets[6],
+		Version:     version,
+		DeviceName:  deviceDets[2],
+		BuildType:   deviceDets[3],
+		Flavour:     deviceDets[5],
+		BuildDate:   deviceDets[6],
+		BuildFormat: buildFormats,
 	}, fullOtaFile, incrementalOtaFile, nil
 }
